284aefb30c12eeb515c7503d88371c6a281c29b9
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-03-16 17:06:06 +0530

    16-03-2024-008

---
+++ Thread_Based_Multi_Tasking/Creating_thread_by_Extending_Thread_class.java
@@ -0,0 +1,34 @@
+package Thread_Based_Multi_Tasking;
+
+
+class Cat extends Thread{
+    public void run(){
+        System.out.println("Meow");
+    }
+}
+
+class Dog extends Thread{
+    public void run(){
+        for(int i = 0;i<5;i++){
+            System.out.println("Bark");
+        }
+        System.out.println("\n");
+    }
+}
+
+
+public class Creating_thread_by_Extending_Thread_class {
+    public static void main(String[] args) {
+        //main thread started
+
+        
+        //thread -1
+        Dog dog = new Dog();
+        dog.start();
+        
+        //thread -2
+        Cat cat = new Cat();
+        cat.start();
+        //if there is a delays, TS will switch to another thread(Line - 12 )
+    }
+}
