b4336316c1c40e8293d3e754f9870d4ad23c6da0
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-04-27 00:58:03 +0530

    27-04-2024-003

---
+++ Applet/ImageApplet.java
@@ -0,0 +1,19 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class ImageApplet extends Applet {
+    private Image image;
+
+    public void init() {
+        // Load the image
+        image = getImage(getDocumentBase(), "Bg.jpg");
+    }
+
+    public void paint(Graphics g) {
+        // Draw the image at (0, 0)
+        g.drawImage(image, 0, 0, this);
+    }
+}
+
+
---
+++ Thread_Based_Multi_Tasking/Join.java
@@ -0,0 +1,37 @@
+public class Join {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Thread 1: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread 1 interrupted");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Thread 2: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread 2 interrupted");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start(); 
+
+        try {
+            thread1.join(); // Wait for thread 1 to finish
+            thread2.join(); // Wait for thread 2 to finish
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+
+        System.out.println("Main thread exiting");
+    }
+}
